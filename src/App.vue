42a726da123ce8c0969d403e10107962038b6c6b
Gerardo Pérez <ISnape.jilc45gmail.com>
2020-06-22 19:28:51 -0500

    Navegación

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <List></List>
+    <Header/>
+
+    <router-link to="/">List</router-link>
+    <router-link to="/detail">Detail</router-link>
+    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import List from './components/Table'
+
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    List
+    Header
+    //Detail
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
